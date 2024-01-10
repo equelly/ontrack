9cbf28e6ee480d291001b8b34e86561509363642
equelly <pustoselov_andrei@mail.ru>
2024-01-10 14:51:54 +0300

    add sets and mashines to admin.show

--- app/Http/Controllers/Admin/Order/ShowController.php
+++ app/Http/Controllers/Admin/Order/ShowController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Order;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Mashine;
 use App\Models\Order;
+use App\Models\Set;
 
 class ShowController extends BaseController
 {
@@ -13,10 +14,12 @@ class ShowController extends BaseController
         $order = Order::FindOrFail($order->id);
         $mashine_sets = Mashine::find($order->mashine_id);
         $orders = Order::all();
+        $mashines = Mashine::all();
+        $sets = Set::all();
         $cur_orders = Order::all()->where('category_id', '=','1')->where('content', '!=', '');
         $exc_orders = Order::all()->where('category_id', '=','2')->where('content', '!=', '');
         $den_orders = Order::all()->where('category_id', '=','3')->where('content', '!=', '');
      
-        return view('admin.order.show', compact('order', 'mashine_sets','cur_orders', 'exc_orders', 'den_orders', 'orders'));
+        return view('admin.order.show', compact('order', 'mashine_sets','cur_orders', 'exc_orders', 'den_orders', 'orders', 'mashines', 'sets'));
     }
 }
