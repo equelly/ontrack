3dbf51350e6ae73eefda466e717104bf453f430e
equelly <pustoselov_andrei@mail.ru>
2025-01-06 13:25:36 +0300

    sidebar  mode

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -67,6 +67,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #8b959e;">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
+      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">sMs</span>
     </a>
 
@@ -74,7 +75,9 @@
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        
+        <div class="image">
+          <img src="" class="img-circle elevation-2" alt="User Image">
+        </div>
         <div class="info">
           <a href="#" class="d-block">{{auth()->user()->name}}</a>
         </div>
@@ -127,11 +130,7 @@
     </div>
   </footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+  
 </div>
 <!-- ./wrapper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
